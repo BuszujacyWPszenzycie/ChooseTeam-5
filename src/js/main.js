64c2a5dd00bf043b6027f32af048f95d90c55eeb
Marcin <marcin.szturminski@gmail.com>
2023-03-14 22:18:06 +0100

    backup before adding background to player div

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,25 @@ const addBtn = document.querySelector('.btn__add')
 const closeBtn = document.querySelector('.modal__close-btn')
 const inputPicture = document.querySelector('.input__picture')
 const fileP = document.querySelector('.file__name')
-const activeBtn = document.querySelector('.btn__approve')
+const approveBtn = document.querySelector('.btn__approve')
+const clearBtn = document.querySelector('.btn__clear')
+const teamBox = document.querySelector('.box__team')
+const modalInputName = document.querySelector('.modal__input-name')
+
+const creatingNewPlayer = () => {
+	let newPlayerDiv = document.createElement('div')
+	newPlayerDiv.classList.add('team__player')
+	teamBox.appendChild(newPlayerDiv)
+	clearModal()
+}
+
+const clearModal = () => {
+	approveBtn.classList.remove('btn__approve-active')
+	clearBtn.classList.remove('btn__clear-active')
+	fileP.textContent = ''
+	modalInputName.value = ''
+	inputPicture.value = ''
+}
 
 const showModal = () => {
 	mainShadow.classList.add('main__shadow-active')
@@ -29,8 +47,11 @@ inputPicture.addEventListener('change', e => {
 	const fileSize = (size / 1000).toFixed(2)
 	const fileNameAndSize = `${fileName} - ${fileSize}KB`
 	fileP.textContent = fileNameAndSize
-	activeBtn.classList.add('btn__approve-active')
+	approveBtn.classList.add('btn__approve-active')
+	clearBtn.classList.add('btn__clear-active')
 })
 
 addBtn.addEventListener('click', showModal)
 closeBtn.addEventListener('click', closeModal)
+approveBtn.addEventListener('click', creatingNewPlayer)
+clearBtn.addEventListener('click', clearModal)
